75da0fcb95b752ce0114ae138f93310e4534d988
Will Hawthorne <willhwt@gmail.com>
2023-04-13 16:39:42 -0400

    Created new page for printer details

---
+++ php/printer_details.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Printer Details</title>
+        <link rel="stylesheet" href="../web/css/styles.css">
+    </head>
+</html>
