94835724928077d2bf1e3c66ea386e244efb4bd2
John Morris <70734743+jmorris335@users.noreply.github.com>
2023-04-25 16:21:59 -0400

    Added filtering options to sensor_data

--- php/sensor_data.php
+++ php/sensor_data.php
@@ -15,8 +15,6 @@
                 include_once ("SQL_functions.php");
                 $conn = connectToServer(to_print: false);
                 $conn->query("USE 3DPrinterDT;");
-
-                
         ?>
         
         <h3> Filter Options </h3>
@@ -52,7 +50,7 @@
                 </div>
                 <div class='row'>
                     <div class='col-25'>
-                        <label for='printer'>Filter by Sensor</label>
+                        <label for='sensor'>Filter by Sensor</label>
                     </div>
                     <div class='col-75'>
                         <select id='sensor' name='sensor'>
@@ -65,7 +63,7 @@
                     </div>
                 </div>
                 <div class ='submit-row'>
-                        <input type="submit" value="Submit">
+                        <input type="submit" value="Apply Filter(s)">
                         <input type="reset" value="Reset">
                         
                 </div>
@@ -77,10 +75,11 @@
             $table_name = "Sensor_Data";
 
             $query = "SELECT Sensor_Data.data_ID, Sensor_Data.timestamp, Sensor_Data.data_name,
-            Sensor_Data.value, Sensor_Data.sensor_ID, Sensor_Data.unit, Sensor.part_ID, Part.printer_ID
-            FROM Sensor_Data, Sensor, Part
+            Sensor_Data.value, Sensor_Data.sensor_ID, Sensor_Data.unit, Part.part_name, Printer.printer_name
+            FROM Sensor_Data, Sensor, Part, Printer
             WHERE Sensor_Data.sensor_ID = Sensor.sensor_ID
-            AND Sensor.part_ID = Part.part_ID";
+            AND Sensor.part_ID = Part.part_ID
+            AND Part.printer_ID = Printer.printer_ID";
 
             if (!empty($_POST['sensor'])) {
                 $sensor_id = $_POST['sensor'];
@@ -96,8 +95,10 @@
             }
             $query = $query." ORDER BY Sensor_Data.timestamp ASC, Sensor_Data.data_ID ASC;";
             
-            $attributes = array("data_ID", "timestamp", "data_name", "value", "sensor_ID", "unit",
-                                "part_ID", "printer_ID");
+            $attributes = array("data_ID", "timestamp", "data_name", "value", "sensor_ID", 
+                                "unit", "part_name", "printer_name");
+            $labels = array("Data ID", "Timestamp", "Name", "Value", "Sensor ID", 
+                                "Unit", "Part Name", "Printer Name");
             $results = $conn->query($query);
             $table = $results->fetch_all(MYSQLI_BOTH);
         ?>
@@ -109,7 +110,7 @@
 
                     printf("<table>");
                     for ($i=0; $i < count($attributes); $i++) {
-                        printf("<th>%s</th>\n", $attributes[$i]);
+                        printf("<th>%s</th>\n", $labels[$i]);
                     }
                     foreach($table as $row) {
                         $entity_id = $row[0];
@@ -125,6 +126,34 @@
                 ?>
             </table>
         </div>
+
+        <script>
+            const part_input = document.getElementById("part");
+            const sensor_input = document.getElementById("sensor");
+            const parts = <?php echo json_encode($parts); ?>;
+            const sensors = <?php echo json_encode($sensors); ?>;
+
+            function changePart(printer_id) {
+                var part_options = "<option value=\"\" disabled selected>Select...</option>";
+                for (i = 0; i < parts.length; i++) {
+                    if (parts[i]["printer_ID"] == printer_id) {
+                        part_options += "\n<option value=" + parts[i]["part_ID"]+ ">" + parts[i]["part_name"] + "</option>";
+                    }
+                }
+                part_input.innerHTML = part_options;
+            }
+
+            function changeSensor(part_id) {
+                var sensor_options = "<option value=\"\" disabled selected>Select...</option>";
+                for (i = 0; i < sensors.length; i++) {
+                    if (sensors[i]["part_ID"] == part_id) {
+                        sensor_options += "\n<option value=" + sensors[i]["sensor_ID"]+ ">" + sensors[i]["sensor_name"] + "</option>";
+                    }
+                }
+                sensor_input.innerHTML = sensor_options;
+            }
+
+        </script>
 
         <?php
             $conn->close();
--- web/nav.php
+++ web/nav.php
@@ -2,17 +2,10 @@
 function printTopNav(string $pagetitle = null) {
     printf('
         <div class="topnav">
-            <div class="dropdown">
-                <button class=dropbtn> Printers
-                    <i class="fa fa-caret-down"></i>
-                </button>
-                <div class="dropdown-content">
-                    <a href="http://localhost:8888/DesignInformatics/php/printer_overview.php">Printer Interface</a>
-                    <a href="http://localhost:8888/DesignInformatics/php/submit_job.php">Submit Job</a>
-                </div>
-            </div>
-            <a href="http://localhost:8888/DesignInformatics/php/Rhyans_Forms/Change_db_Entity.php">Manage Database</a>
+            <a href="http://localhost:8888/DesignInformatics/php/printer_overview.php">Printer Interface</a>
+            <a href="http://localhost:8888/DesignInformatics/php/submit_job.php">Submit Job</a>
             <a href="http://localhost:8888/DesignInformatics/php/sensor_data.php">Data Overview</a>
+            <a href="http://localhost:8888/DesignInformatics/php/Rhyans_Forms/Change_db_Entity.php">Manage Database</a>
             <div class="dropdown">
                 <button class=dropbtn> Tools
                     <i class="fa fa-caret-down"></i>
