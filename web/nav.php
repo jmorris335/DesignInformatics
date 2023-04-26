8ba6f90bb6d84b2c9310a4f6b6cf634910141d03
John Morris <70734743+jmorris335@users.noreply.github.com>
2023-04-26 13:56:34 -0400

    Updated nav bar

--- web/nav.php
+++ web/nav.php
@@ -3,7 +3,15 @@ function printTopNav(string $pagetitle = null) {
     printf('
         <div class="topnav">
             <a href="http://localhost:8888/DesignInformatics/php/printer_overview.php">Printer Interface</a>
-            <a href="http://localhost:8888/DesignInformatics/php/submit_job.php">Submit Job</a>
+            <div class="dropdown">
+                <button class=dropbtn> Print Jobs
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="http://localhost:8888/DesignInformatics/php/submit_job.php">Submit Job</a>
+                    <a href="http://localhost:8888/DesignInformatics/php/print_report.php">Add Job Report</a>
+                </div>
+            </div>
             <a href="http://localhost:8888/DesignInformatics/php/sensor_data.php">Data Overview</a>
             <a href="http://localhost:8888/DesignInformatics/php/Rhyans_Forms/Change_db_Entity.php">Manage Database</a>
             <div class="dropdown">
