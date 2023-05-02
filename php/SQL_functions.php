a0da31565b7c56976a46aa6e61619444eab29c8e
John Morris <70734743+jmorris335@users.noreply.github.com>
2023-05-02 10:55:52 -0400

    Status fidelity refinement

--- php/SQL_functions.php
+++ php/SQL_functions.php
@@ -254,11 +254,11 @@ function setPrinterStatus(int $printer_id, string $new_status, mysqli $conn) {
         }
     }
     $date = new DateTime("now", new DateTimeZone("America/New_York"));
-    $date_string = $date->format("Y-m-d H:i:s");
+    $date_string = $date->format("Y-m-d H:i:s.v");
     $insert_cols = array("printer_ID", "timestamp", "is_available", "is_connected", "is_busy", "needs_service", "has_error");
     $insert_vals = array($printer_id, $date_string, $is_available, $is_connected, $is_busy, $needs_service, $has_error);
     $query = makeInsertQuery("Printer_State", $insert_cols, array($insert_vals));
-    //printf($query);
+    // printf($query);
     $conn->query($query);
 }
 
